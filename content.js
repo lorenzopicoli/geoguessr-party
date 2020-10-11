b4b63f500396fc50f392037c399861281331324a
Elcineyjr <elcineyjr@gmail.com>
2020-10-10 22:45:26 -0300

    feat: find link and add party button

--- content.js
+++ content.js
@@ -1,3 +1,22 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href");
+const targetNode = document.getElementsByTagName('body')[0];
 
-console.log(firstHref);
+const config = { attributes: true, childList: true, subtree: true };
+
+const findStartGameButton = () => {
+  var copyLink = $("input[name='copy-link']");
+
+  if(copyLink.length > 0){
+    observer.disconnect(); 
+  }
+
+  var nextRoundButton = $("button[data-qa='close-round-result']");
+  
+  copyLink.parent().append('<button>Iniciar party</button>');
+  
+  console.log(copyLink);
+  console.log(nextRoundButton);
+};
+
+const observer = new MutationObserver(findStartGameButton);
+
+observer.observe(targetNode, config);
