70d2aa046ac48dc5999bf0d83c3148edfd7e9138
Elcineyjr <elcineyjr@gmail.com>
2020-10-11 19:54:19 -0300

    feat: add message sender with party link

--- src/backgroundPage.ts
+++ src/backgroundPage.ts
@@ -1,10 +1,16 @@
-import { browser } from "webextension-polyfill-ts";
+import { browser } from 'webextension-polyfill-ts'
 
 // Listen for messages sent from other parts of the extension
-browser.runtime.onMessage.addListener((request: { popupMounted: boolean }) => {
+browser.runtime.onMessage.addListener(
+  (request: { popupMounted: boolean; partyLink: string }) => {
     // Log statement if request.popupMounted is true
     // NOTE: this request is sent in `popup/component.tsx`
-    if (request.popupMounted) {
-        console.log("backgroundPage notified that Popup.tsx has mounted.");
+    if (request.partyLink) {
+      console.log('bó começar a festa')
     }
-});
+
+    if (request.popupMounted) {
+      console.log('backgroundPage notified that Popup.tsx has mounted.')
+    }
+  },
+)
--- src/content/index.tsx
+++ src/content/index.tsx
@@ -2,15 +2,19 @@ import $ from 'jquery'
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import styled from 'styled-components'
+import { browser } from 'webextension-polyfill-ts'
 // import '../scss/app.scss'
 
-const PartyModeToggle = (): JSX.Element => (
+const PartyModeToggle = (props: {
+  onClick: () => Promise<void>
+}): JSX.Element => (
   <div>
     <label className="checkbox">
       <input
         type="checkbox"
         className="checkbox__input"
         name="show-game-settings"
+        onClick={props.onClick}
       />
       <span className="checkbox__mark checkbox__mark--dark"></span>
       <span className="game-settings__checkbox-main-label">Party mode</span>
@@ -27,17 +31,21 @@ const ContainerStyled = styled.div`
   margin-top: 20px;
 `
 
-const Container = (): JSX.Element => (
+const Container = (props: { challengeLink: string }): JSX.Element => (
   <ContainerStyled>
-    <PartyModeToggle />
+    <PartyModeToggle
+      onClick={(): Promise<void> =>
+        browser.runtime.sendMessage({ partyLink: props.challengeLink })
+      }
+    />
   </ContainerStyled>
 )
 
 const injectHtml: MutationCallback = (_mutationList, observer): void => {
-  const copyLink = $('input[name="copy-link"]')
+  const challengeLink = $('input[name="copy-link"]').val()
   const startChallengeButton = $('button[data-qa="start-challenge-button"]')
 
-  if (copyLink.length === 0 || startChallengeButton.length === 0) {
+  if (!challengeLink || startChallengeButton.length === 0) {
     return
   }
 
@@ -49,9 +57,12 @@ const injectHtml: MutationCallback = (_mutationList, observer): void => {
 
   startChallengeButton.before(root)
 
-  ReactDOM.render(<Container />, document.getElementById('gparty-root'))
+  ReactDOM.render(
+    <Container challengeLink={challengeLink.toString()} />,
+    document.getElementById('gparty-root'),
+  )
 
-  console.log(copyLink)
+  console.log(challengeLink)
   console.log(startChallengeButton)
 }
 
